12347cfc47585406cf5d6a2f0a05ffffeff66f2b
pgg <pggeldenhuys@gmail.com>
2023-04-18 09:24:17 +0200

    add app3, wrapper and test

--- src/app/app3.cy.ts
+++ src/app/app3.cy.ts
@@ -2,7 +2,7 @@ import { App3WrapperComponent } from './app3-wrapper.component';
 import { provideRouter } from '@angular/router';
 import { App3Component } from './app3.component';
 
-it('mounting', () => {
+it('wrapper mount should route to component', () => {
   cy.mount(App3WrapperComponent, {
     providers: [
       provideRouter([
